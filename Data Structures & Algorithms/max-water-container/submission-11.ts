a9186f60be6500eb9cc7d83cd9a0b253f46602ec
Damian Danner <50763041+ddanner97@users.noreply.github.com>
2026-09-09 22:06:34 -0500

    Add: max-water-container - submission-11

---
+++ Data Structures & Algorithms/max-water-container/submission-11.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights: number[]): number {
+
+        let max: number = 0
+        let lptr: number = 0
+        let rptr: number = heights.length - 1
+
+        while (lptr < rptr) {
+
+            let width: number = rptr - lptr
+            max = Math.max(max, width * Math.min(heights[rptr], heights[lptr]))
+
+            if (heights[lptr] >= heights[rptr]) {
+                rptr--
+            } else {
+                lptr++
+            }
+
+        }
+
+        return max
+
+    }
+}
